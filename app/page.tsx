332e81a1b9dffafd49e5e4b9752142a754500faf
Vithushan <63534975+ACuteWoof@users.noreply.github.com>
2024-02-29 14:36:54 +0000

    layout update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import Right from "./sections/right"
+import Left from "./sections/left"
+
 export default function Home() {
-  return <main className="h-screen bg-black"></main>
+  return <main className="h-screen bg-background grid lg:grid-cols-3">
+    <div className="lg:col-span-1">
+      <Left />
+    </div>
+    <div className="lg:col-span-2">
+      <Right />
+    </div>
+  </main>
 }
