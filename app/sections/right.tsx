9c33965410f50ccd312c9948221340248c1ea682
ACuteWoof <sutharshanvithushan@gmail.com>
2024-03-01 01:30:36 +0530

    forward slash

--- app/sections/right.tsx
+++ app/sections/right.tsx
@@ -42,7 +42,7 @@ export default function Right() {
     ]
 
     return <div className="bg-primary-foreground lg:h-screen w-full lg:overflow-auto p-8 lg:p-24 flex flex-col gap-8">
-        <span className={"text-lg font-medium text-muted-foreground lowercase " + titleFont.className}>// Projects</span>
+        <span className={"text-lg font-medium text-muted-foreground lowercase " + titleFont.className}>&#47;&#47; Projects</span>
         <div className="flex flex-col gap-8">
             {
                 projects.map((project, index) => <Piece key={index} {...project} className="m-4" />)
