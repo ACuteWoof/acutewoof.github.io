0dda5ddbac80946641b71bcfb3afa2b4a1be5a93
Vithushan <63534975+ACuteWoof@users.noreply.github.com>
2024-05-25 03:42:49 +0530

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -2,6 +2,14 @@ import { ProjectInfo } from "./types";
 
 export const projects: ProjectInfo[] = [
   {
+    title: "Woof OS",
+    description:
+      "An Arch Linux respin, with the Qtile window manager, calamares installer to install my entire Arch Linux rice.",
+    image: "/images/woof-os.png",
+    link: "https://os.lewoof.xyz",
+    tags: ["Linux", "OS", "Python", "Shell Script"],
+  },
+  {
     title: "E-Commerce Dashboard for GSL Wholesalers LTD",
     description:
       "The frontend for the e-commerce dashboard built specifically for GSL Wholesalers LTD. The frontend template code is available publicly on Github.",
@@ -9,7 +17,15 @@ export const projects: ProjectInfo[] = [
     image: "/images/gsl.png",
     tags: ["Next", "React", "Typescript", "TailwindCSS", "Shadcn UI"],
   },
- {
+  {
+    title: "Startpage",
+    description:
+      "A startpage for your browser, with Spotify intergration, a todo list, and a note keeper.",
+    link: "https://startpage-lemon.vercel.app/",
+    image: "/images/startpage.png",
+    tags: ["Next", "React", "Typescript", "TailwindCSS"],
+  },
+  {
     title: "Obsek Gallery",
     description: "Amateur astrophotography gallery.",
     link: "https://acutewoof.is-a.dev/gallery",
@@ -30,12 +46,4 @@ export const projects: ProjectInfo[] = [
     image: "/images/kreatis.png",
     tags: ["Next", "React", "Typescript", "TailwindCSS", "Framer Motion"],
   },
-  {
-    title: "Startpage",
-    description:
-      "A startpage for your browser, with Spotify intergration, a todo list, and a note keeper.",
-    link: "https://startpage-lemon.vercel.app/",
-    image: "/images/startpage.png",
-    tags: ["Next", "React", "Typescript", "TailwindCSS"],
-  }
 ];
