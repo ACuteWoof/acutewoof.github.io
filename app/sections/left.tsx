3ea0e014260647d68f5e525e73600be522570eab
ACuteWoof <sutharshanvithushan@gmail.com>
2024-07-11 00:25:17 +0530

    rm matrix

--- app/sections/left.tsx
+++ app/sections/left.tsx
@@ -1,115 +1,127 @@
-import Link from "next/link"
-import { mono as titleFont } from "../fonts"
+import Link from "next/link";
+import { mono as titleFont } from "../fonts";
 import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-} from "@/app/components/ui/accordion"
-import { Badge } from "../components/ui/badge"
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/app/components/ui/accordion";
+import { Badge } from "../components/ui/badge";
 
 export default function Left() {
-    return <div className="h-fit lg:h-screen w-full py-12 px-8 lg:p-24 lg:pb-12 lg:overflow-auto">
-        <article className={"prose dark:prose-invert"}>
-            <h1 className={titleFont.className + " mb-3"}>Vithushan</h1>
-	    <Link href="https://github.com/acutewoof" className="exception"><Badge variant="secondary">github.com/acutewoof</Badge></Link>
-            <p>
-                A developer with a passion for design and technology.
-            </p>
-            <span>
-                <h3>Skills</h3>
-                <ul>
-                    <li>
-                        <Accordion type="single" collapsible>
-                            <AccordionItem className="border-none" value="styles">
-                                <AccordionTrigger>Styling, Animations & Components</AccordionTrigger>
-                                <AccordionContent>
-                                    <ul>
-                                        <li>Framer Motion</li>
-                                        <li>Ant Design</li>
-                                        <li>Shadcn UI</li>
-                                        <li>MUI</li>
-                                        <li>Daisy UI</li>
-                                        <li>TailwindCSS</li>
-                                    </ul>
-                                </AccordionContent>
-                            </AccordionItem>
-                        </Accordion></li>
-                    <li>
-                        <Accordion type="single" collapsible>
-                            <AccordionItem className="border-none" value="frameworks">
-                                <AccordionTrigger>Frameworks</AccordionTrigger>
-                                <AccordionContent>
-                                    <ul>
-                                        <li>Next</li>
-                                        <li>React</li>
-                                    </ul>
-                                </AccordionContent>
-                            </AccordionItem>
-                        </Accordion></li>
-                    <li>
-                        <Accordion type="single" collapsible>
-                            <AccordionItem className="border-none" value="platforms">
-                                <AccordionTrigger>Platforms</AccordionTrigger>
-                                <AccordionContent>
-                                    <ul>
-                                        <li>Vercel</li>
-                                        <li>Firebase</li>
-                                        <li>Github</li>
-                                    </ul>
-                                </AccordionContent>
-                            </AccordionItem>
-                        </Accordion></li>
-                    <li>
-                        <Accordion type="single" collapsible>
-                            <AccordionItem className="border-none" value="languages">
-                                <AccordionTrigger>Languages</AccordionTrigger>
-                                <AccordionContent>
-                                    <ul>
-                                        <li>Typescript</li>
-                                        <li>Javascript</li>
-                                        <li>Python</li>
-                                        <li>HTML & CSS</li>
-                                    </ul>
-                                </AccordionContent>
-                            </AccordionItem>
-                        </Accordion></li></ul>
-            </span>
-            <span>
-                <h3>Experience</h3>
-                <ul>
-                    <li>Web developer at <Link target="_blank" href="https://kreatis.io">Kreatis</Link>.</li>
-                </ul>
-            </span>
-           <span>
-                <h3>Donate</h3>
-                <ul>
-                    <li className="text-wrap break-all">
-                        Monero: AvKjLpQzaE8Aj8A4vgRyuUYsX7GdGo8SddcmMiHf1H2L55CGMUxKUmZUovZtWRkw9LuE4Zr6PRzQg9ivMeueGwa38YHx7u
-                    </li>
-                    <li>
-                        Buymeacoffee: <Link target="_blank" href="https://buymeacoffee.com/acutewoof">
-                            acutewoof
-                        </Link>
-                    </li>
-                </ul>
-            </span>
-            <span>
-                <h3>Contact</h3>
-                <ul>
-                    <li>
-                        Email: <Link target="_blank" href="mailto:sutharshanvithushan@gmail.com">
-                            sutharshanvithushan@gmail.com
-                        </Link>
-                    </li>
-                    <li>
-                        Matrix: <Link target="_blank" href="https://matrix.to/#/@acutewoof:mx.lewoof.xyz">
-                            @acutewoof:mx.lewoof.xyz
-                        </Link>
-                    </li>
-                </ul>
-            </span>
- 
-        </article>
-    </div >
+  return (
+    <div className="h-fit lg:h-screen w-full py-12 px-8 lg:p-24 lg:pb-12 lg:overflow-auto">
+      <article className={"prose dark:prose-invert"}>
+        <h1 className={titleFont.className + " mb-3"}>Vithushan</h1>
+        <Link href="https://github.com/acutewoof" className="exception">
+          <Badge variant="secondary">github.com/acutewoof</Badge>
+        </Link>
+        <p>A developer with a passion for design and technology.</p>
+        <span>
+          <h3>Skills</h3>
+          <ul>
+            <li>
+              <Accordion type="single" collapsible>
+                <AccordionItem className="border-none" value="styles">
+                  <AccordionTrigger>
+                    Styling, Animations & Components
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <ul>
+                      <li>Framer Motion</li>
+                      <li>Ant Design</li>
+                      <li>Shadcn UI</li>
+                      <li>MUI</li>
+                      <li>Daisy UI</li>
+                      <li>TailwindCSS</li>
+                    </ul>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </li>
+            <li>
+              <Accordion type="single" collapsible>
+                <AccordionItem className="border-none" value="frameworks">
+                  <AccordionTrigger>Frameworks</AccordionTrigger>
+                  <AccordionContent>
+                    <ul>
+                      <li>Next</li>
+                      <li>React</li>
+                    </ul>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </li>
+            <li>
+              <Accordion type="single" collapsible>
+                <AccordionItem className="border-none" value="platforms">
+                  <AccordionTrigger>Platforms</AccordionTrigger>
+                  <AccordionContent>
+                    <ul>
+                      <li>Vercel</li>
+                      <li>Firebase</li>
+                      <li>Github</li>
+                    </ul>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </li>
+            <li>
+              <Accordion type="single" collapsible>
+                <AccordionItem className="border-none" value="languages">
+                  <AccordionTrigger>Languages</AccordionTrigger>
+                  <AccordionContent>
+                    <ul>
+                      <li>Typescript</li>
+                      <li>Javascript</li>
+                      <li>Python</li>
+                      <li>HTML & CSS</li>
+                    </ul>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </li>
+          </ul>
+        </span>
+        <span>
+          <h3>Experience</h3>
+          <ul>
+            <li>
+              Web developer at{" "}
+              <Link target="_blank" href="https://kreatis.io">
+                Kreatis
+              </Link>
+              .
+            </li>
+          </ul>
+        </span>
+        <span>
+          <h3>Donate</h3>
+          <ul>
+            <li className="text-wrap break-all">
+              Monero:
+              AvKjLpQzaE8Aj8A4vgRyuUYsX7GdGo8SddcmMiHf1H2L55CGMUxKUmZUovZtWRkw9LuE4Zr6PRzQg9ivMeueGwa38YHx7u
+            </li>
+            <li>
+              Buymeacoffee:{" "}
+              <Link target="_blank" href="https://buymeacoffee.com/acutewoof">
+                acutewoof
+              </Link>
+            </li>
+          </ul>
+        </span>
+        <span>
+          <h3>Contact</h3>
+          <ul>
+            <li>
+              Email:{" "}
+              <Link target="_blank" href="mailto:sutharshanvithushan@gmail.com">
+                sutharshanvithushan@gmail.com
+              </Link>
+            </li>
+          </ul>
+        </span>
+      </article>
+    </div>
+  );
 }
